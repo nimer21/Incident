5a9eefb2cba4564c6f49b0ac448ec6bcd599db79
Alsayed Nimer <nimer.alsayed@sos-palestine.org>
2024-12-27 22:14:30 +0200

    create app folder

--- backend/app/server.js
+++ backend/app/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
-const incidentRoutes = require('./routes/incidentRoutes');
-const userRoutes = require('./routes/userRoutes');
+const connectDB = require('../config/db');
+const incidentRoutes = require('../routes/incidentRoutes');
+const userRoutes = require('../routes/userRoutes');
 const cookieParser = require('cookie-parser');
 
 require('dotenv').config();
